90bfc46749f8fc4b90146b70a6185580022b1996
Opeyemi <yemi.otu@outlook.com>
2023-09-19 19:44:51 -0600

    if with (else if)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let user_points = 50
 if(user_points>=75){
     console.log("Congratulations!!!");
+} else if (user_points<=50){
+    console.log("Good Job! You are almost there");
 } else {
-    console.log("Great Job! Try more one time")
+    console.log("Great Job! Try more one time");
 }
