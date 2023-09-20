2fa9c97fd3db4596e2867f0b25b77bde370648ca
Opeyemi <yemi.otu@outlook.com>
2023-09-19 21:42:29 -0600

    if with (nested if) not working

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-let user_points = 50
-if(user_points>=75){
+// Variables
+let user_points = 95;
+let is_member = true;
+let user_name = "BossMove56";
+
+
+// My conditional statements
+if (user_name = "Alex") {
+    console.log("Welcome to BibleMan Game!!!");
+}
+  else if (user_name != "Alex") {
+    console.log("Join the Community and Learn more about God!!!");
+}
+  else if( is_member = false || user_points < 10 ) {
+    console.log("Intruder Alert! Please Leave!");
+}
+  else if ( user_points >= 75 ){
     console.log("Congratulations!!!");
-} else if (user_points<=50){
+} else if ( user_points <= 50) {
     console.log("Good Job! You are almost there");
+} else if ( is_member = true && user_points >= 90 ){
+    console.log("Congratulations! You Won a Iphone20!!");
 } else {
     console.log("Great Job! Try more one time");
-}
+} 
+
+
+
